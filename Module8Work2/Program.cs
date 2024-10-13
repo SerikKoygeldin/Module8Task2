73edc05ee6f80c179723400efd863f956761d397
Name <email@example.ru>
2024-10-13 15:07:53 +0500

    Добавьте файлы проекта.

---
+++ Module8Work2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Module8Work2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dirName = @"C:\\CSharp\\1";
+            DirectoryInfo dirInfo = new DirectoryInfo(dirName);
+
+            try
+            {
+                long dirSize = DirSize(dirInfo);
+                Console.WriteLine(dirSize);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }        
+        }
+
+        public static long DirSize(DirectoryInfo d)
+        {
+            long size = 0;
+            FileInfo[] fis = d.GetFiles();
+            foreach (FileInfo fi in fis)
+            {
+                size += fi.Length;
+            }
+
+            DirectoryInfo[] dis = d.GetDirectories();
+            foreach (DirectoryInfo Di in dis)
+            {
+                size += DirSize(d);
+            }
+
+            return size;
+        }
+    }
+}
